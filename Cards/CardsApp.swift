ba682c6e8b419404e6e76be0d028a1a153ef8d2a
neon443 <69979447+neon443@users.noreply.github.com>
2025-08-09 22:51:32 -0400

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by neon443 on 09/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
